69acd5978dfdc54853f8a710969fad1cd58079db
Ng Yik Phang <ngyikp@gmail.com>
2022-04-23 20:03:44 +0100

    Detect mouse movement

--- src/drawing/DrawingApp.js
+++ src/drawing/DrawingApp.js
@@ -1,7 +1,43 @@
 // @flow strict
 
-import './DrawingApp.module.css';
+import {useCallback, useState} from 'react';
+
+import styles from './DrawingApp.module.css';
 
 export default function DrawingApp(): React$Node {
-	return <div className="App">Hello world</div>;
+	const [mouseMoveCoordinates, setMouseMoveCoordinates] =
+		useState<?[number, number]>(null);
+
+	const onMouseMove = useCallback(
+		(ev: SyntheticMouseEvent<HTMLDivElement>) => {
+			const rect = ev.currentTarget.getBoundingClientRect();
+
+			setMouseMoveCoordinates([
+				parseInt(ev.clientX - rect.left, 10),
+				parseInt(ev.clientY - rect.top, 10),
+			]);
+		},
+		[setMouseMoveCoordinates]
+	);
+
+	const onMouseLeave = useCallback(
+		(ev: SyntheticMouseEvent<>) => {
+			setMouseMoveCoordinates(null);
+		},
+		[setMouseMoveCoordinates]
+	);
+
+	return (
+		<div
+			className={styles.fullscreen}
+			onMouseMove={onMouseMove}
+			onMouseLeave={onMouseLeave}
+		>
+			{mouseMoveCoordinates != null ? (
+				<>
+					{mouseMoveCoordinates[0]}, {mouseMoveCoordinates[1]}
+				</>
+			) : null}
+		</div>
+	);
 }
